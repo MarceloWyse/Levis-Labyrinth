bed553ded03ed5db1166c0f78cd13fd6574a8d98
MarceloWyse <marceloamaral88@gmail.com>
2023-09-22 07:40:00 -0300

    video

--- intro_video.gd
+++ intro_video.gd
@@ -1,5 +1,11 @@
 extends VideoStreamPlayer
 
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		stop()
+		get_tree().change_scene_to_file("res://menu.tscn")
+		
+
 func _ready():
 	play()
 	await self.finished
